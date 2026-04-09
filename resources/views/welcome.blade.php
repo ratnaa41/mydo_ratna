dceeb2518003ff3c223b92fa00006b2a75a8c1da
ratnaa41 <ihaan137@gmail.com>
2026-04-09 23:34:51 +0600

    Full re-upload of all files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MYDO · tasks · budget · notes</title>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-<script src="{{ asset('js/app.js') }}" defer></script>
+<link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+<script src="{{ asset('public/js/app.js') }}" defer></script>
 </head>
 <body>
     <!-- Notification Container -->
