acdf5e9f8a0b3253fa74367e3a028a0c94e92ed9
ratnaa41 <ihaan137@gmail.com>
2026-04-09 17:58:25 +0600

    Full re-upload of all files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MYDO · tasks · budget · notes</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <!-- Notification Container -->
